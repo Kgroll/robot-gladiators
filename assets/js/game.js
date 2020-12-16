f03e742355082bc6d4305c4eed6e3590fdc5993a
Kristen Groller <groller@rogers.com>
2020-12-15 21:43:51 -0500

    fight function added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,24 @@
 var playerName = window.prompt("What is your robot's name?");
-// Note the lack of quotation marks around playerName
-console.log(playerName);
-console.log("This logs a string, good for leaving youself a message");
-//this witll do math and log 20
-console.log(10 + 10);
-//what is this?
-console.log("Our robot's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+// You can also log multiple values at once like this
+
+console.log(playerName, playerAttack, playerHealth);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enenyAttack = 12;
+
+
+
 
 
 
 
 // this creates a function named fight
 
-function fight() {
-    window.alert("The fight has begun!");
-}
-//fight();
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
 
