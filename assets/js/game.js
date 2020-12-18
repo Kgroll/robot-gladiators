681a1d2ac7448ec2c8de4abd4efea360d9605528
Kristen Groller <groller@rogers.com>
2020-12-17 22:11:21 -0500

    loops added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,9 @@
 
-
+//Game States
+// "WIN" -Player robot has defeated all enemy robots
+//      *Fight all enemy robots
+//      *Defeat each enemy robot
+// "LOS" -Player robot's health is zero or less
 
 
 
@@ -8,11 +12,24 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10
 // You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth, playerMoney);
 
-console.log(playerName, playerAttack, playerHealth);
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+
 var enemyHealth = 50;
 var enenyAttack = 12;
+
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index ");
+}
 
 
 window.alert("Welcome to Robot Gladiators!");
@@ -66,7 +83,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 
     // if no (false), ask question again by running fight() again
     else {
-        var promptFight = window.prompt ("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+        //var promptFight = window.prompt ("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
         console.log(promptFight);
     }
 } else {
